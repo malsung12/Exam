a42f9642b8f5b629c8239ef56f42507b31d51dd0
박성원 <malsung12@gmail.com>
2016-12-09 11:07:49 +0900

    1번 레이아웃 완료

--- app/src/main/java/com/example/a403/exam/MainActivity.java
+++ app/src/main/java/com/example/a403/exam/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.a403.exam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
+import android.graphics.Color;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("놀이동산 예약시스템");
     }
 }
